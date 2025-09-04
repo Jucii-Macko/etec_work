dd3fc9c1b03f35b28d038e6ef242e11e788db3a2
Pen Khemaraktumpoi <khemaraktumpoirpen1@gmail.com>
2025-09-04 22:40:24 +0700

    Add new route and show detail

--- resources/views/showDetail.blade.php
+++ resources/views/showDetail.blade.php
@@ -26,7 +26,7 @@
         <p class="text-xl font-bold text-red-500 mb-3">Sale: ${{ $p['sale_price'] }}</p>
         <img src="{{ $p['thumbnail'] }}" width="100" alt=""
                 class="rounded border border-gray-300">
-        <p class="text-xm text-gray-700 mt-0 mb-2">{{ $p['color'] }}</p>
+        <p class="text-xm text-gray-900 mt-0 mb-2">{{ $p['color'] }}</p>
         <p class="text-xm text-gray-700 mb-2">Size: {{ $p['size'] }}</p>
 
         <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full md:w-auto">
